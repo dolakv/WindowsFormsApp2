efc3439e4120a6583e9fbae712037815f69c574d
Vojtěch Dolák <dolakv.05@spst.cz>
2022-02-09 08:31:35 +0100

    Součet

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -16,5 +16,17 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            label1.Text = Convert.ToString(a + b);
+        }
     }
 }
